99a93275d71fe2295683d0b7c6f613529215e64e
Vadim220409 <107919314+Vadim220409@users.noreply.github.com>
2023-08-10 18:49:14 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+
+
+// ex 1
+
+
+// let number = 1;
+
+// while (number <= 10) {
+//   console.log(number);
+//   number++;
+// }
+
+
+// ex 2
+
+
+// for (let number = 2; number <= 20; number += 2) {
+//     if (number % 2 !== 0) {
+//       continue; 
+//     }
+//     console.log(number);
+//   }
+
+
+// ex 3
+
+
+// for (let i = 1; i <= 10; i++) {
+//     let result = 7 * i;
+//     console.log(`7 * ${i} = ${result}`);
+// }
+
+
+// ex 4
+
+
+// let numbers = [1, 2, 3, 4, 5];
+// let index = 0;
+
+// while (index < numbers.length) {
+//     console.log(numbers[index]);
+//     index++;
+// }
+
+
+// ex 5
+
+
+// const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < numbersArray.length; i++) {
+//   if (numbersArray[i] === 7) {
+//     break;
+//   }
+//   console.log(numbersArray[i]);
+// }
